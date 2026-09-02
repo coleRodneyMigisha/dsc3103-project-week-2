20ea39b8c2ebaf7133252b30c062dc8b961b065f
Cole Rodney Migisha <kingcole1205@gmail.com>
2026-09-03 00:52:35 +0300

    rules

--- check.py
+++ check.py
@@ -1,8 +1,16 @@
-from src.validate.rules import rule_positive_price
+from src.validate.rules import rule_positive_price, rule_duplicate_ids, rule_duplicate_rows, rule_valid_date, rule_missing_market, rule_known_commodity
 import pandas as pd
 
 df = pd.read_csv("data/raw/prices.csv")
 
 neg_prices = rule_positive_price(df)
+dupe = rule_duplicate_ids(df)
+dub = rule_duplicate_rows(df)
+val = rule_valid_date(df)
+missing_market = rule_missing_market(df)
 
 print(neg_prices)
+print(dupe)
+print(dub)
+print(val)
+print(missing_market)
--- src/validate/rules.py
+++ src/validate/rules.py
@@ -1,4 +1,32 @@
+import pandas as pd
+
 def rule_positive_price(df):
     neg_prices = df[df['price'] < 0].copy()
     neg_prices["reason"] = "Negative price"
     return neg_prices
+
+def rule_duplicate_ids(df):
+    duplicate_ids = df[df['id'].duplicated()].copy()
+    duplicate_ids["reason"] = "Duplicate id"
+    return duplicate_ids
+
+def rule_duplicate_rows(df):
+    duplicate_rows = df[df.duplicated()].copy()
+    duplicate_rows["reason"] = "Duplicate row"
+    return duplicate_rows
+
+def rule_valid_date(df):
+    valid_dates = df.copy()
+    parsed_dates = pd.to_datetime(valid_dates['date'], errors='coerce', format='%Y-%m-%d')
+    valid_dates["valid_dates"] = parsed_dates.notna()
+    invalid_dates = valid_dates[~valid_dates["valid_dates"]]
+    invalid_dates["reason"] = "Invalid date"
+    return invalid_dates
+
+def rule_missing_market(df):
+    missing_markets = df[df['market'].isnull()].copy()
+    missing_markets["reason"] = "Missing market"
+    return missing_markets
+
+def rule_known_commodity(df):
+    pass
